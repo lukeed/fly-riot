0751ea0b4da9ecfff2d00c490a3c2422a1cd7126
Luke Edwards <luke.edwards05@gmail.com>
2016-10-20 11:35:31 +0200

    update tests; using fly

--- test/fixtures/bar.js
+++ test/fixtures/bar.js
@@ -1,3 +1,3 @@
 riot.tag2('test', '<p>Hello World</p>', 'test,[riot-tag="test"],[data-is="test"]{ display: block; }', '', function(opts) {
-    console.log('hello');
+		console.log('hello');
 });
--- test/index.js
+++ test/index.js
@@ -1,20 +1,35 @@
-var riot = require('../');
-var test = require('tape').test;
-var read = require('fs').readFileSync;
+'use strict';
 
-test('fly-riot', function (t) {
-  t.plan(3);
+const join = require('path').join;
+const test = require('tape').test;
+const Fly = require('fly');
 
-  var file1 = read('test/a.tag');
-  var code1 = read('test/a.js', 'utf8');
+const dir = join(__dirname, 'fixtures');
+const tmp = join(__dirname, 'tmp');
 
-  riot.call({
-    filter: function(name, fn) {
-      var results = fn(file1);
+test('fly-riot', t => {
+	t.plan(3);
 
-      t.equal(name, 'riot', 'add riot filter to fly');
-      t.equal(results.ext, '.js', 'compiled to ".js" file');
-      t.equal(results.code, code1, 'compiled to javascript');
-    }
-  });
+	const fly = new Fly({
+		plugins: [{
+			func: require('../')
+		}],
+		tasks: {
+			a: function * () {
+				t.ok('riot' in fly, 'attach the `riot()` plugin to fly');
+
+				yield this.source(`${dir}/foo.tag`).riot().target(tmp);
+
+				const str = yield this.$.read(`${tmp}/foo.js`, 'utf8');
+				const want = yield this.$.read(`${dir}/bar.js`, 'utf8');
+
+				t.ok(str, 'create a `.js` file');
+				t.equal(str, want, 'compile the `.tag` file correctly');
+
+				yield this.clear(tmp);
+			}
+		}
+	});
+
+	fly.start('a');
 });
